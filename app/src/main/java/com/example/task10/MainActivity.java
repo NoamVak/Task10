82ba71f3812a52185af2ebe239010df7ce7b24c9
NoamVak <noamvak765@gmail.com>
2020-10-04 20:15:05 +0300

    need to continue

--- app/src/main/java/com/example/task10/MainActivity.java
+++ app/src/main/java/com/example/task10/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.task10;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
@@ -16,8 +17,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     Spinner cont;
     ListView countries;
     TextView cap,theme,lang,pop;
-    String contA[]=new String[4];
-    String countryA[]=new String[7];
+    String [] contA;
+    int row;
+    String [][] countryA={{"Kenya","Ethiopia","Ghana","Nigeria","Morocco","Sudan","Egypt"},
+            {"Japan","India","China","Thailand","South Korea","Vietnam","Nepal"},
+            {"United states","Canada","Brazil","Mexico","Argentina","Cuba","Peru"},
+            {"Germany","United Kingdom","Italy","France","Spain","Netherlands","Sweden"}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,29 +36,47 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         theme=(TextView)findViewById(R.id.theme);
         lang=(TextView)findViewById(R.id.lang);
 
+        contA=getResources().getStringArray(R.array.cont);
+
+
         cont.setOnItemSelectedListener(this);
         countries.setOnItemClickListener(this);
+
         countries.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
-        ArrayAdapter<String> spAdp= new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,contA);
+        ArrayAdapter<String> spAdp = new ArrayAdapter<String>(this,
+                R.layout.support_simple_spinner_dropdown_item,contA);
         cont.setAdapter(spAdp);
 
-        ArrayAdapter<String> lvAdp= new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,countryA);
-        countries.setAdapter(lvAdp);
+        countries.setAdapter(null);
+    }
+
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long rowid) {
+        if(pos==0){
+            countries.setAdapter(null);
+
+        }
+        else{
+            ArrayAdapter<String> lvAdp= new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item,countryA[pos-1]);
+            countries.setAdapter(lvAdp);
+            row=pos;
+            countries.setOnItemClickListener(this);
+        }
+
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+    public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
 
-    }
-
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
+
 
     }
 }
